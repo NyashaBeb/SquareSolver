5fc6721a0f4542cb6588efe14595c7bb70711255
NyashaBeb <gasin1106@gmail.com>
2026-08-20 15:06:19 +0300

    EnumBetterNames

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
+enum flags{
+    INFSOL = 999999,
+    NOSOL = 999990,
+    ONESOL = 999991,
+    TWOSOL = 999992
+};
+
 /*Using Functions*/
 int BinomialSolver(double a, double b, double c, double* x1, double* x2);
 int LinearSolver(double b, double c, double* x);
+
 int main()
 {
 /*Program Start*/
@@ -27,34 +35,31 @@ int main()
     }
 /*Printing*/
     switch (Flag){
-        case EOF:
+        case INFSOL:
             printf("InfinitySolves\n");
             break;
-        case 0:
+        case NOSOL:
             printf("NoSolves\n");
             break;
-        case 1:
+        case ONESOL:
             printf("x = %lf\n", x1);
             break;
-        case 2:
+        case TWOSOL:
             printf("x1 = %lf\tx2 = %lf\n", x1, x2);
             break;
     }
     return 0;
 }
-/*Solves Binomials Arg: a, b, c Solves: x1, x2 Out: 0 NoSolves, 1 OneSolve, 2 TwoSolves, EOF InfSolves*/
+/*Solves Binomials Arg: a, b, c Solves: x1, x2*/
 int BinomialSolver (double a, double b, double c, double* x1, double* x2)
 {
     double disk = b * b - 4 * a * c;
 
     if (disk < 0) {
-        *x1 = -1;
-        *x2 = -1;
         return 0;
     }
     else if (disk == 0) {
         *x1 = -b / (2 * a);
-        *x2 = -1;
         return 1;
     }
     else {
@@ -63,12 +68,11 @@ int BinomialSolver (double a, double b, double c, double* x1, double* x2)
         return 2;
     }
 }
-/*Solves Linears Arg: b, c Solve: x Out: 0 NoSolves, 1 OneSolve*/
+/*Solves Linears Arg: a, b Solve: x*/
 int LinearSolver(double a, double b, double* x)
 {
-    if (a == 0){
-        *x = -1;
-        return (b == 0)? EOF : 0;
+    if (a == 0) {
+        return (b == 0)? INFSOL : 0;
     }
     else {
         *x = -b / a;
